aac55745b27b086137f86b0d0b8ad9e6fc8dc94d
Zein Setyo Budi I <zeinsetyobudi@yahoo.com>
2021-12-17 16:24:18 +0700

    add dispatcher

--- app/src/main/java/id/zeinsetyobudi/githubuserlist/apicontroller/FollowingApiConfig.kt
+++ app/src/main/java/id/zeinsetyobudi/githubuserlist/apicontroller/FollowingApiConfig.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class FollowingApiConfig {
     companion object{
@@ -12,6 +13,9 @@ class FollowingApiConfig {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
